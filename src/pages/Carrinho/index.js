f3a164bd683fad1187d971033784a3981c579509
Guilherme ZF <guilhermefragiacomo1@gmail.com>
2023-10-31 00:57:35 -0300

    first commit

---
+++ src/pages/Carrinho/index.js
@@ -0,0 +1,86 @@
+import { Button, Snackbar, InputLabel, Select, MenuItem } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
+import { useState } from 'react';
+import { Container, Voltar, TotalContainer, PagamentoContainer } from './styles';
+import { useCarrinhoContext } from 'common/context/Carrinho';
+import Produto from 'components/Produto';
+import { useHistory } from 'react-router-dom';
+import { usePagamentoContext } from 'common/context/Pagamento';
+
+function Carrinho() {
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const { carrinho, valorTotalCarrinho } = useCarrinhoContext();
+  const { tiposPagamento, formaPagamento, mudarFormaPagamento } = usePagamentoContext();
+  const history = useHistory();
+
+  return (
+    <Container>
+      <Voltar onClick={() => history.goBack()} />
+      <h2>
+        Carrinho
+      </h2>
+      {valorTotalCarrinho}
+      {carrinho.map(produto => (
+        <Produto
+          {...produto}
+          key={produto.id}
+        />
+      ))}
+      <PagamentoContainer>
+        <InputLabel> Forma de Pagamento </InputLabel>
+        <Select
+          value={formaPagamento.id}
+          onChange={(event) => mudarFormaPagamento(event.target.value)}
+        >
+          {tiposPagamento.map(pagamento => (
+            <MenuItem value={pagamento.id} key={pagamento.id}>
+              {pagamento.nome}
+            </MenuItem>
+          ))}
+        </Select>
+      </PagamentoContainer>
+      <TotalContainer>
+        <div>
+          <h2>Total no Carrinho: </h2>
+          <span>R$ </span>
+        </div>
+        <div>
+          <h2> Saldo: </h2>
+          <span> R$ </span>
+        </div>
+        <div>
+          <h2> Saldo Total: </h2>
+          <span> R$ </span>
+        </div>
+      </TotalContainer>
+      <Button
+        onClick={() => {
+          setOpenSnackbar(true);
+        }}
+        color="primary"
+        variant="contained"
+      >
+        Comprar
+      </Button>
+      <Snackbar
+        anchorOrigin={
+          {
+            vertical: 'top',
+            horizontal: 'right'
+          }
+        }
+        open={openSnackbar}
+        onClose={() => setOpenSnackbar(false)}
+      >
+        <MuiAlert
+          onClose={() => setOpenSnackbar(false)}
+          severity="success"
+        >
+          Compra feita com sucesso!
+        </MuiAlert>
+      </Snackbar>
+    </Container>
+  )
+}
+
+export default Carrinho;
